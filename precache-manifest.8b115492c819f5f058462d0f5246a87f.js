a030c228a7b0d23ac00ef38bb90bc4d5a036d024
ColumbusK <banjag954@naver.com>
2019-07-21 23:25:43 +0900

    Updates

---
+++ precache-manifest.8b115492c819f5f058462d0f5246a87f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f8f21163776c2d7e26a9675db2573bb",
+    "url": "/08_movieApp/index.html"
+  },
+  {
+    "revision": "c07e8e06bb1ce0123b59",
+    "url": "/08_movieApp/static/css/main.ceac89f8.chunk.css"
+  },
+  {
+    "revision": "cc5e4bcf973d41ae4f71",
+    "url": "/08_movieApp/static/js/2.a9ae5d1e.chunk.js"
+  },
+  {
+    "revision": "c07e8e06bb1ce0123b59",
+    "url": "/08_movieApp/static/js/main.e94f67a1.chunk.js"
+  },
+  {
+    "revision": "5b13ecbab60e5dc428ae",
+    "url": "/08_movieApp/static/js/runtime~main.f0024380.js"
+  }
+]);
